724bbfe95709686a10d64ba6d6839372a87b2e56
wass <wassimbs088@gmail.com>
2024-10-07 20:39:47 +0100

    more error handling

--- C_Sockets/client.c
+++ C_Sockets/client.c
@@ -1,35 +1,38 @@
-#include <stdio.h>
 #include <stdlib.h>
 #include <sys/socket.h>
 #include <sys/types.h>
+#include <stdio.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
-#include <unistd.h>
-int main(int argc, char **argv) {
 
-    int network_socket = socket(AF_INET,SOCK_STREAM,0);
+int main(int argc, char **argv) 
+{
+    int network_socket;
+    if ((network_socket = socket(AF_INET, SOCK_STREAM, 0)) == -1 ) {
+        fprintf(stderr, "socket creation error");
+        return -1;
+    }
     
     struct sockaddr_in server_address = {
-        .sin_addr.s_addr = inet_addr("127.0.0.1"),
         .sin_family = AF_INET,
-        .sin_port = htons(5000)
+        .sin_port = htons(12345)
     };
+    inet_aton("127.0.0.1", & (server_address.sin_addr));
 
-    int connection_status = connect(network_socket, (struct sockaddr *) &server_address, sizeof(server_address));
+    int connection_status;
+    if ((connection_status = connect(network_socket, (struct sockaddr *) &server_address, sizeof(server_address))) == -1) {
+        fprintf(stderr, "error can't connect");
+    }
 
-    char buffer[32];
-    recv(network_socket , &buffer, sizeof(buffer), 0);
-    
-    printf("%s\n", buffer);
-
-    /* FOR UDP COMMUNICATIONS , USE THIS : */
+    char server_response[256];
+    recv(network_socket, &server_response, sizeof(server_response),0);
+    printf("%s", server_response);
 
     /*
      sendto(sockfd, message, strlen(message), 0, (const struct sockaddr *) &servaddr, sizeof(servaddr));
 
      recvfrom(sockfd, buffer, sizeof(buffer), 0, (struct sockaddr *) &servaddr, &len);
     */
-    close(network_socket);
 
     return 0;
 }
--- C_Sockets/server.c
+++ C_Sockets/server.c
@@ -1,36 +1,43 @@
 #include <stdlib.h>
-#include <stdio.h>
-#include <unistd.h>
-#include <sys/types.h>
 #include <sys/socket.h>
+#include <sys/types.h>
+#include <stdio.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
-int main(int argc, char **argv) {
-    
-    int server_socket = socket(AF_INET, SOCK_STREAM, 0);
 
+int main(int argc, char const *argv[])
+{
+    int server_socket;
+    if ((server_socket = socket(AF_INET, SOCK_STREAM, 0)) == -1 ) {
+        fprintf(stderr, "socket creation error");
+        return -1;
+    }
+    
     struct sockaddr_in server_address = {
-        .sin_addr.s_addr = inet_addr("127.0.0.1"),
         .sin_family = AF_INET,
-        .sin_port = htons(5000)
+        .sin_port = htons(12345)
     };
+    inet_aton("127.0.0.1", & (server_address.sin_addr));
 
-    bind(server_socket, (struct sockaddr *) &server_address, sizeof(server_address));
+    if ((bind(server_socket, (struct sockaddr *) &server_address, sizeof(server_address))) == -1) {
+        fprintf(stderr, "error can't bind");
+        return -1;
+    }
 
-    listen(server_socket, 5);
+    listen(server_socket,5);
 
-    int client_socket = accept(server_socket,NULL,NULL);
-    
-    char message[32] = "hello , it's server";
-    send(server_socket, message, sizeof(message), 5);
+    int client_socket_channel;
+    if ((client_socket_channel = accept(server_socket,NULL,NULL)) == -1) {
+        fprintf(stderr, "error can't connect client");
+        return -1;
+    } else {
+        printf("client connected \n");
+    }
+    char sent_data[20] = "hello wassim";
+    send(client_socket_channel, sent_data, sizeof(sent_data), 0);
 
-    /* FOR UDP COMMUNICATIONS , USE THIS : */
 
-    /*
-     sendto(sockfd, message, strlen(message), 0, (const struct sockaddr *) &servaddr, sizeof(servaddr));
-
-     recvfrom(sockfd, buffer, sizeof(buffer), 0, (struct sockaddr *) &servaddr, &len);
-    */
-    close(server_socket);
+    close(client_socket_channel);    
     return 0;
 }
+
